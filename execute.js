ab27591c387bc09cce4732137dcba2514d3470d2
Nicola <nicola.sansom@slimmingworld.co.uk>
2021-02-03 18:50:57 +0000

    Update execute script

--- execute.js
+++ execute.js
@@ -1,11 +1,24 @@
-require('dotenv').config();
-const {SmartThingsClient, BearerTokenAuthenticator} = require('@smartthings/core-sdk')
-const client = new SmartThingsClient(new BearerTokenAuthenticator(process.env.PAT));
-const alfy = require('alfy');
-const input = await alfy.input;
+require('dotenv').config()
 
-const executeScene = () => {
-  client.scenes.execute(input).catch(() => '');
+const { SmartThingsClient, BearerTokenAuthenticator } = require('@smartthings/core-sdk')
+const client = new SmartThingsClient(new BearerTokenAuthenticator(process.env.PAT))
+const alfy = require('alfy')
+const input = alfy.input
+const format = input.match(/([\w+]+)/g)
+const [id, capability, command] = format
+
+export const excuteDeviceCommand = (id, command) => {
+  client.devices.execute(input, [{ capability: 'switch', command }]).catch(() => '')
 }
 
-executeScene();
+const executeScene = (deviceId) => {
+  client.scenes.execute(deviceId).catch(() => '')
+}
+
+if (capability === 'scenes') {
+  executeScene(id)
+}
+
+if (capability === 'switch') {
+  executeScene(id, command)
+}
